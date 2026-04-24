2782bbe8ef915b5bab2a368c0e52806f75fdf194
PuruMohite <107110840+PuruMohite@users.noreply.github.com>
2026-04-24 22:09:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,25 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	//my repeated sorting and finding the sum of two elements
+	if(arr.length == 1){
+		return 0;
+	}
+	arr.sort();
+	let cost = 0;
+    while(arr.length != 1){
+        let sum = arr[0]+arr[1];
+        cost += sum;
+        arr.shift();
+        arr[0] = sum;
+        // let i=0;
+        // while(arr[i]<arr[i+1] && i<arr.length-1){
+        //     [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
+        //     i++;
+        // }
+        arr.sort();
+    }
+    console.log(cost);
 }
 
 module.exports=mincost;
